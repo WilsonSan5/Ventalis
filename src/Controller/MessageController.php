d00f70cb9031492dbd960e1f8f41240fe4cb73a7
WilsonSan5 <wilson.san57@gmail.com>
2023-04-17 01:28:35 +0200

    messagerie v0.8 added visual, back-end fonctionnel

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -15,14 +15,45 @@ use App\Repository\MessageRepository;
 class MessageController extends AbstractController
 {
     #[Route('/message', name: 'app_message')]
-    public function index(): Response
+    public function index(MessageRepository $messageRepository, MessagerieRepository $messagerieRepository): Response
     {
         $user = $this->getUser();
-
         $messageries = $user->getMessageries(); //tableau d'objet messagerie
-        
+
         return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+            'messageries' => $messageries,
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/message/new', name: 'app_message_new', methods: ['POST'])]
+    public function new(MessagerieRepository $messagerieRepository, MessageRepository $messageRepository): Response
+    {
+        $user = $this->getUser();
+        $messageries = $user->getMessageries();
+
+        if ($_POST['objet'] && $_POST['message']) {
+            $newObjet = $_POST['objet'];
+            $newContenu = $_POST['message'];
+
+            $newMessage = new Message;
+            $newMessagerie = new Messagerie;
+
+            $newMessagerie->setObjet($newObjet);
+            $newMessagerie->addUser($this->getUser());
+            $newMessagerie->addUser($this->getUser()->getConseiller());
+
+
+            $newMessage->setAuthor($this->getUser());
+            $newMessage->setContenu($newContenu);
+            $newMessage->setMessagerie($newMessagerie);
+
+            $messagerieRepository->save($newMessagerie, true);
+            $messageRepository->save($newMessage, true);
+
+            return $this->redirectToRoute('app_message_show', ['id' => $newMessagerie->getId()]);
+        }
+        return $this->render('message/index.html.twig', [
             'messageries' => $messageries,
             'user' => $user
         ]);
@@ -31,11 +62,15 @@ class MessageController extends AbstractController
     #[Route('/message/{id}', name: 'app_message_show', methods: ['GET'])]
     public function show(Messagerie $messagerie): Response
     {
+        $user = $this->getUser();
+        $messageries = $user->getMessageries();
         $messages = $messagerie->getMessages();
-        
+
         return $this->render('message/show.html.twig', [
-            'messagerie' => $messagerie,
-            'messages' => $messages
+            'allMessageries' => $messageries,
+            'currentMessageries' => $messagerie,
+            'messages' => $messages,
+            'user' => $user
         ]);
     }
 
@@ -44,13 +79,15 @@ class MessageController extends AbstractController
     {
         $messages = $messagerie->getMessages();
 
-        if($_POST['message']){
+        if ($_POST['message']) {
             $newMessage = new Message;
             $newMessage->setContenu($_POST['message']);
             $newMessage->setAuthor($this->getUser());
             $newMessage->setMessagerie($messagerie);
 
-            $messageRepository->save($newMessage,true);
+            $messageRepository->save($newMessage, true);
+
+            return $this->redirectToRoute('app_message_show', ['id' => $messagerie->getId()]);
         }
 
         return $this->render('message/show.html.twig', [
@@ -58,4 +95,5 @@ class MessageController extends AbstractController
             'messages' => $messages
         ]);
     }
+
 }
